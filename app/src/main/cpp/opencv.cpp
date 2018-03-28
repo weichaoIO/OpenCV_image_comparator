fb424ba5ee852611d79dec5b0d93148fbd74b3c9
chao.wei <chao.wei@jsxfedu.com>
2018-03-28 17:36:50 +0800

    添加【峰值信噪比】和【结构相似性】算法用于计算相似度。

--- app/src/main/cpp/opencv.cpp
+++ app/src/main/cpp/opencv.cpp
@@ -1,11 +1,98 @@
 #include <jni.h>
 #include <string>
 
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>  // Gaussian Blur
+#include <opencv2/core/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
+#include <opencv2/highgui/highgui.hpp>  // OpenCV window I/O
+
+using namespace std;
+using namespace cv;
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_io_weichao_opencv_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
     std::string hello = "初始化完毕";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT jdouble JNICALL
+Java_io_weichao_opencv_MainActivity_getPSNR(JNIEnv *env, jobject /* this */, jlong addr1,
+                                            jlong addr2) {
+    Mat &I1 = *(Mat *) addr1;
+    Mat &I2 = *(Mat *) addr2;
+    Mat s1;
+    absdiff(I1, I2, s1);       // |I1 - I2|
+    s1.convertTo(s1, CV_32F);  // cannot make a square on 8 bits
+    s1 = s1.mul(s1);           // |I1 - I2|^2
+
+    Scalar s = sum(s1);        // sum elements per channel
+
+    double sse = s.val[0] + s.val[1] + s.val[2]; // sum channels
+
+    if (sse <= 1e-10) // for small values return zero
+        return 0;
+    else {
+        double mse = sse / (double) (I1.channels() * I1.total());
+        double psnr = 10.0 * log10((255 * 255) / mse);
+        return psnr;
+    }
+}
+
+extern "C"
+JNIEXPORT jdouble JNICALL
+Java_io_weichao_opencv_MainActivity_getSSIM(JNIEnv *env, jobject /* this */, jlong addr1,
+                                            jlong addr2) {
+    const double C1 = 6.5025, C2 = 58.5225;
+    /***************************** INITS **********************************/
+    int d = CV_32F;
+
+    Mat &i1 = *(Mat *) addr1;
+    Mat &i2 = *(Mat *) addr2;
+    Mat I1, I2;
+    i1.convertTo(I1, d);            // cannot calculate on one byte large values
+    i2.convertTo(I2, d);
+
+    Mat I2_2 = I2.mul(I2);        // I2^2
+    Mat I1_2 = I1.mul(I1);        // I1^2
+    Mat I1_I2 = I1.mul(I2);        // I1 * I2
+
+    /*************************** END INITS **********************************/
+
+    Mat mu1, mu2;                   // PRELIMINARY COMPUTING
+    GaussianBlur(I1, mu1, Size(11, 11), 1.5);
+    GaussianBlur(I2, mu2, Size(11, 11), 1.5);
+
+    Mat mu1_2 = mu1.mul(mu1);
+    Mat mu2_2 = mu2.mul(mu2);
+    Mat mu1_mu2 = mu1.mul(mu2);
+
+    Mat sigma1_2, sigma2_2, sigma12;
+
+    GaussianBlur(I1_2, sigma1_2, Size(11, 11), 1.5);
+    sigma1_2 -= mu1_2;
+
+    GaussianBlur(I2_2, sigma2_2, Size(11, 11), 1.5);
+    sigma2_2 -= mu2_2;
+
+    GaussianBlur(I1_I2, sigma12, Size(11, 11), 1.5);
+    sigma12 -= mu1_mu2;
+
+    ///////////////////////////////// FORMULA ////////////////////////////////
+    Mat t1, t2, t3;
+
+    t1 = 2 * mu1_mu2 + C1;
+    t2 = 2 * sigma12 + C2;
+    t3 = t1.mul(t2);                 // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
+
+    t1 = mu1_2 + mu2_2 + C1;
+    t2 = sigma1_2 + sigma2_2 + C2;
+    t1 = t1.mul(t2);                 // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
+
+    Mat ssim_map;
+    divide(t3, t1, ssim_map);        // ssim_map =  t3./t1;
+
+    Scalar mssim = mean(ssim_map);   // mssim = average of ssim map
+
+    return mssim.val[0] + mssim.val[1] + mssim.val[2];
 }
--- app/src/main/java/io/weichao/opencv/MainActivity.java
+++ app/src/main/java/io/weichao/opencv/MainActivity.java
@@ -12,17 +12,9 @@ import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
-import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfFloat;
-import org.opencv.core.MatOfInt;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     // Used to load the 'opencv' library on application startup.
@@ -69,66 +61,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         comPareHist(mat11, mat22);
     }
 
-    private Mat mMat0;
-    private MatOfInt mChannels[];
-    private MatOfInt mHistSize;
-    private int mHistSizeNum = 25;
-    private MatOfFloat mRanges;
-    private Scalar mColorsRGB[];
-    private Point mP1;
-    private Point mP2;
-    private float mBuff[];
-
-    public Mat procSrc2GrayJni(Mat srcMat, int type) {
-        Mat grayMat = new Mat();
-        Imgproc.cvtColor(srcMat, grayMat, type);//转换为灰度图
-        // Imgproc.HoughCircles(rgbMat, gray,Imgproc.CV_HOUGH_GRADIENT, 1, 18);
-        // //霍夫变换找园
-        mChannels = new MatOfInt[]{new MatOfInt(0), new MatOfInt(1), new MatOfInt(2)};
-        mBuff = new float[mHistSizeNum];
-        mHistSize = new MatOfInt(mHistSizeNum);
-        mRanges = new MatOfFloat(0f, 256f);
-        mMat0 = new Mat();
-        mColorsRGB = new Scalar[]{new Scalar(200, 0, 0, 255), new Scalar(0, 200, 0, 255), new Scalar(0, 0, 200, 255)};
-        mP1 = new Point();
-        mP2 = new Point();
-
-
-        Mat rgba = srcMat;
-        Size sizeRgba = rgba.size();
-        Mat hist = new Mat(); //转换直方图进行绘制
-        int thikness = (int) (sizeRgba.width / (mHistSizeNum + 10) / 5);
-        if (thikness > 5) thikness = 5;
-        int offset = (int) ((sizeRgba.width - (5 * mHistSizeNum + 4 * 10) * thikness) / 2);
-        // RGB
-        for (int c = 0; c < 3; c++) {
-            Imgproc.calcHist(Arrays.asList(rgba), mChannels[c], mMat0, hist, mHistSize, mRanges);
-            Core.normalize(hist, hist, sizeRgba.height / 2, 0, Core.NORM_INF);
-            hist.get(0, 0, mBuff);
-            for (int h = 0; h < mHistSizeNum; h++) {
-                mP1.x = mP2.x = offset + (c * (mHistSizeNum + 10) + h) * thikness;
-                mP1.y = sizeRgba.height - 1;
-                mP2.y = mP1.y - 2 - (int) mBuff[h];
-                Imgproc.line(rgba, mP1, mP2, mColorsRGB[c], thikness);
-            }
-        }
-
-        return rgba;
-    }
-
     /**
      * 比较来个矩阵的相似度
      *
      * @param srcMat
-     * @param desMat
+     * @param dstMat
      */
-    public void comPareHist(Mat srcMat, Mat desMat) {
+    public void comPareHist(Mat srcMat, Mat dstMat) {
+        if (srcMat.empty() || dstMat.empty()) {
+            return;
+        }
+
+        double psnr = getPSNR(srcMat.getNativeObjAddr(), dstMat.getNativeObjAddr());
+        Log.e(TAG, "【峰值信噪比】相似度：" + psnr);
+
+        double ssim = getSSIM(srcMat.getNativeObjAddr(), dstMat.getNativeObjAddr());
+        Log.e(TAG, "【结构相似性】相似度：" + ssim);
+
         srcMat.convertTo(srcMat, CvType.CV_32F);
-        desMat.convertTo(desMat, CvType.CV_32F);
-        float target = (float) Imgproc.compareHist(srcMat, desMat, Imgproc.CV_COMP_CORREL);
-        Log.e(TAG, "相似度：" + target);
-        Toast.makeText(this, "相似度：" + target, Toast.LENGTH_SHORT).show();
+        dstMat.convertTo(dstMat, CvType.CV_32F);
+        float hist = (float) Imgproc.compareHist(srcMat, dstMat, Imgproc.CV_COMP_CORREL);
+        Log.e(TAG, "【直方图】相似度：" + hist);
     }
 
     public native String stringFromJNI();
+
+    public native double getPSNR(long mat1, long mat2);
+
+    public native double getSSIM(long mat1, long mat2);
 }
